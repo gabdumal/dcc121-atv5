0abf62edbe3b4e9e5fd8041e6ee0d85da2f79cdb
Gabriel Malosto <professional@gabdumal.com>
2022-07-27 13:22:54 -0300

    Cria item de lista e o adiciona à lista de pessoas

--- index.js
+++ index.js
@@ -13,8 +13,23 @@ formPessoa.addEventListener("submit", submitListener);
 function submitListener(event) {
   event.preventDefault();
 
+  // Estado
   const nome = formPessoa.nome.value;
   estado.pessoas.push(nome);
+
+  // DOM
+  const listaPessoas = document.querySelector("#participantes");
+  const linha = document.createElement("li");
+  const btnRemover = document.createElement("button");
+  btnRemover.textContent = "❌";
+  const nomePessoa = document.createElement("span");
+  nomePessoa.textContent = nome;
+  linha.appendChild(btnRemover);
+  linha.appendChild(nomePessoa);
+  listaPessoas.appendChild(linha);
+
+  // Limpa formulário
+  formPessoa.reset();
 
   console.log(estado);
 }
